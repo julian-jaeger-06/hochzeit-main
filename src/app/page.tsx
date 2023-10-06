2d049d066b74bbe2436b356ea083c04e891298da
Adham <32067614+azakiio@users.noreply.github.com>
2023-10-06 21:13:12 +0200

    remove extra body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,14 @@ import Navbar from "@/components/Navbar/Navbar";
 import Hero from "@/components/Hero/Hero";
 import Countdown from "@/components/Countdown/Countdown";
 import Ablauf from "@/components/Ablauf/Ablauf";
-import Location from "@/components/Location/Location"
+import Location from "@/components/Location/Location";
 import Uebernachtung from "@/components/Uebernachtung/Uebernachtung";
 import Footer from "@/components/Footer/Footer";
 import Anmeldung from "@/components/Anmeldung/Anmeldung";
 import bg from "@/assets/background_paper.jpeg";
 import Image from "next/image";
 
-
 export default function Home() {
-
   const links = [
     { label: "Countdown", url: "#countdown" },
     { label: "Ablauf", url: "#ablauf" },
@@ -21,7 +19,7 @@ export default function Home() {
   ];
 
   return (
-    <body >
+    <>
       <Navbar links={links} />
       <main className="mainwrapper">
         <Image
@@ -32,13 +30,13 @@ export default function Home() {
           className="fixed inset-0 -z-10 w-screen h-screen object-cover"
         />
         <Hero />
-        <section id='countdown' className="scroll-smooth	scroll-m-24">
+        <section id="countdown" className="scroll-smooth	scroll-m-24">
           <Countdown />
         </section>
-        <section id='ablauf' className="scroll-m-24">
+        <section id="ablauf" className="scroll-m-24">
           <Ablauf />
         </section>
-        <section id='location' className="scroll-m-24">
+        <section id="location" className="scroll-m-24">
           <Location />
         </section>
         <section id="uebernachtung" className="scroll-m-24">
@@ -49,7 +47,6 @@ export default function Home() {
         </section>
       </main>
       <Footer />
-    </body>
-
+    </>
   );
 }
